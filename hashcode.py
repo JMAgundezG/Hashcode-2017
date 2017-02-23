aae54d4910333902f1ec0b78575370863fe44ed3
Pablo Tato <pabtatoramos@gmail.com>
2017-02-23 19:53:13 +0100

    Carga archivo

--- hashcode.py
+++ hashcode.py
@@ -3,7 +3,15 @@
 
 INFINITE_LATENCY = 501
 cache_size = None
+n_videos = None
+n_endpoints = None
+n_requests = None
+n_caches = None
 
+videos = None
+endpoints = None
+caches = None
+requests = None
 
 class endpoint(object):
     def __init__(self, datacenter_latencia, caches_latencia):
@@ -21,3 +29,27 @@ class request(object):
         self.times = times
         self.endpoint_id = endpoint_id
         self.video_id = video_id
+
+
+def read_file(filename):
+    with open(filename, 'r') as fin:
+        n_videos, n_endpoints, n_requests, n_caches, cache_size = [
+            int(x) for x in fin.readline().split()]
+
+        videos = [int(v) for v in fin.readline().split()]
+
+        caches = [cache() for _ in range(n_caches)]
+
+        endpoints = []
+        for _ in range(n_endpoints):
+            dc_latency, n_caches_endpoint = [int(x) for x in fin.readline().split()]
+            endpoint_caches = [INFINITE_LATENCY for x in range(n_caches)]
+            for _ range(n_caches_endpoint):
+                i, latency = [int(x) for x in fin.readline().split()]
+                endpoint_caches[i] = latency
+            endpoints.append(endpoint(dc_latency, endpoint_caches))
+
+        requests = []
+        for _ in range(n_requests):
+            video_id, endpoint_id, n_peticiones = [int(x) for x in fin.readline().split()]
+            requests.append(n_peticiones, endpoint_id, video_id)
