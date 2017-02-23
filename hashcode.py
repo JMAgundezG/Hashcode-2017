8c2e6aee4e06c91d58c269ba2c759ce30f5032ba
DANIEL <dsagradoigl@gmail.com>
2017-02-23 18:57:58 +0100

    añadido archivo de eso es asi

---
+++ hashcode.py
@@ -0,0 +1,8 @@
+class polla(object):
+    def __init__(self, pito, jaja):
+        self.pito = pito
+        self.jaja = jaja
+
+
+polla1 = polla("me la suda", "jaja")
+print(polla1.pito, polla1.jaja)
