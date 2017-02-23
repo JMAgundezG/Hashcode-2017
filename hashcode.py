eee250f7747071d6043e69410105095c259f32f5
Pablo Tato <pabtatoramos@gmail.com>
2017-02-23 19:20:32 +0100

    Shebang

--- hashcode.py
+++ hashcode.py
@@ -1,8 +1,2 @@
-class polla(object):
-    def __init__(self, pito, jaja):
-        self.pito = pito
-        self.jaja = jaja
-
-
-polla1 = polla("me la suda", "jaja")
-print(polla1.pito, polla1.jaja)
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
